e942917fecbaeac335b1987d5c981578908a038c
Fabricio S Werneck <fabricioswerneck@gmail.com>
2019-10-16 17:33:22 +0300

    add white_bread dependency

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule StudentSystem.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:plug_cowboy, "~> 1.0"}
+      {:plug_cowboy, "~> 1.0"},
+      {:white_bread, "~> 4.1.1", only: [:dev, :test]}
     ]
   end
 
@@ -54,7 +55,7 @@ defmodule StudentSystem.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end

14e5ab6b01a7dcfd3ddda537790fa3a417c1756b
Fabricio S Werneck <fabricioswerneck@gmail.com>
2019-10-26 19:01:12 +0300

    change app.py to render home template

--- application/app.py
+++ application/app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
+app = Flask(
+    __name__.partition('.')[0],
+    static_folder='../static')
 
-app = Flask(__name__)
+# would be private in a real life application
+app.secret_key = 'a3ws4e6d5rgt7hy8uj9@#ZXE$VTBYw4es5dr6tfgyih'
+
 
 @app.route("/")
-def hello_world():
-    return 'Hello, world!'
+def home():
+    return render_template('home.html.j2')

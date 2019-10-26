2b973510376457b688a63ac16c669731a1de314f
Hông Duc TA <hong-duc.ta@protonmail.com>
2019-10-26 17:53:39 +0300

    feat : cycle 3 pass

--- TDD/authentication.py
+++ TDD/authentication.py
@@ -1,7 +1,15 @@
-
+import json
 
 def authentication(user,password):
-    result = False
-    if (user == "user" and password == "goodPassword"):
-        result = True
-    return result
+    with open('db.json') as db:
+        data = json.load(db)
+        result = False
+        userFound = False
+        i = 0
+        while (userFound == False and i < len(data)):
+            if (user == data[i]['user']):
+                userFound = True
+                if (password == data[i]['password']):
+                    result = True
+            i = i + 1
+        return result

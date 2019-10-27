f4c3acc6ce3e398c746e2538b7dc8cc3d5a5229c
Fabricio S Werneck <fabricioswerneck@gmail.com>
2019-10-27 19:06:17 +0200

    implement FS01 - passing

--- application/app.py
+++ application/app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from typing import Optional
+
+from flask import Flask, render_template, session
+
+from application.users import views
+from application.users.models import User, Role
 
 app = Flask(
     __name__.partition('.')[0],
@@ -7,7 +12,18 @@ app = Flask(
 # would be private in a real life application
 app.secret_key = 'a3ws4e6d5rgt7hy8uj9@#ZXE$VTBYw4es5dr6tfgyih'
 
+app.register_blueprint(views.users)
+
 
 @app.route("/")
 def home():
+    current_user: Optional[User] = session.get('current_user')
+
+    if current_user is not None:
+        return {
+            Role.Student: views.student_home,
+            Role.Professor: views.professor_home,
+            Role.Staff: views.staff_home
+        }[current_user['role']]()
+
     return render_template('home.html.j2')
--- application/users/views.py
+++ application/users/views.py
@@ -1,21 +1,48 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session, flash, redirect
+
+from .models import User
 
 
-users = Blueprint('users', __name__, url_prefix='users/')
+users = Blueprint('users', __name__, url_prefix='/users')
 
 
-@users.route('/', methods=['GET'])
-def index():
-    pass
+@users.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    cursor = User.select().where(User.username == username).execute()
+
+    try:
+        u: User = cursor.iterate()
+
+        if u.password == password:
+            session['current_user'] = {'id': u.pk, 'role': u.role}
+            flash('Welcome back!')
+
+        else:
+            flash('Invalid credentials.', 'error')
+
+    except StopIteration:
+        flash('No such user.', 'error')
+
+    return redirect('/')
+
+
+@users.route('/logout')
+def logout():
+    session.pop('current_user', None)
+    flash('Logged out successfully.')
+    return redirect('/')
 
 
 def student_home():
-    return render_template('student_home.html.j2')
+    return render_template('users/student_home.html.j2')
 
 
 def professor_home():
-    return render_template('professor_home.html.j2')
+    return render_template('users/professor_home.html.j2')
 
 
 def staff_home():
-    return render_template('staff_home.html.j2')
+    return render_template('users/staff_home.html.j2')

6bf50fac2c71cf7c8b807604a69853ade4bb9cde
Fabricio S Werneck <fabricioswerneck@gmail.com>
2019-10-27 19:19:44 +0200

    add additional scenario

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -11,7 +11,12 @@ class TestUsers(unittest.TestCase):
         self.assertIn(b'Student Homepage', response.data)
 
     def test_login_with_invalid_credentials_should_show_error_message(self):
-        credentials = {'username': 'anything is good', 'password': "doesn't matter"}
+        credentials = {'username': 'student', 'password': "wrong"}
+        response = test_client.post('/users/login', data=credentials, follow_redirects=True)
+
+        self.assertIn(b'Invalid credentials', response.data)
+
+        credentials = {'username': 'non-existent', 'password': "can even be empty"}
         response = test_client.post('/users/login', data=credentials, follow_redirects=True)
 
         self.assertIn(b'Invalid credentials', response.data)

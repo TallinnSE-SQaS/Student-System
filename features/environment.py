3b605f51a972bb01fec4ce88b19492efa2b55f01
Fabricio S Werneck <fabricioswerneck@gmail.com>
2019-11-24 16:29:36 +0200

    final version of environment.py

--- features/environment.py
+++ features/environment.py
@@ -4,7 +4,7 @@ from application.users.models import User, Role
 from application.courses.models import Course, CourseDependency, CourseEnrollment
 
 
-def before_scenario(context, scenario):
+def before_feature(context, feature):
     User.bulk_create([
         User(pk=1, username='student1', password='some-password', role=Role.Student),
         User(pk=2, username='student2', password='some-password', role=Role.Student),
@@ -14,9 +14,9 @@ def before_scenario(context, scenario):
     ])
 
     Course.bulk_create([
-        Course(pk=1, name='Basic Programming', professor_id=4, capacity=2, student_count=2),
-        Course(pk=2, name='Advanced Programming', professor_id=4, capacity=2, student_count=1),
-        Course(pk=3, name='Documentation', professor_id=4, capacity=2, student_count=1),
+        Course(pk=1, name='Basic Programming', professor_id=4, capacity=2),
+        Course(pk=2, name='Advanced Programming', professor_id=4, capacity=2),
+        Course(pk=3, name='Documentation', professor_id=4, capacity=2),
     ])
 
     CourseDependency.create(pk=1, dependant=2, dependee=1)
@@ -28,13 +28,24 @@ def before_scenario(context, scenario):
         CourseEnrollment(student_id=2, course=3),
     ])
 
+    context.base_url = 'http://localhost:5000'
     context.browser = WebDriver()
-    context.browser.get('http://localhost:5000')
 
 
-def after_scenario(context, scenario):
+def after_feature(context, feature):
     for model in [User, CourseEnrollment, CourseDependency, Course]:
         model.drop_table()
         model.create_table()
 
     context.browser.close()
+
+
+def before_scenario(context, scenario):
+    if scenario.name == 'Student tries to enroll in a course that requires another course':
+        CourseEnrollment.delete().where(CourseEnrollment.student == 3).execute()
+
+    context.browser.get(context.base_url)
+
+
+def after_scenario(context, scenario):
+    context.browser.delete_all_cookies()
